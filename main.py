00da37d41d552b262ed11703f2da3aa58f4f2c9f
arif <arifur.tuc@gmail.com>
2023-11-16 15:09:09 +0100

    Add links to news

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 api_key = os.getenv("NEWS_API_KEY")
 
 # Construct the URL for the News API request using the API key
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-10-15&sortBy"
+url = ("https://newsapi.org/v2/everything?q=tesla&sortBy"
        "=publishedAt&apiKey=") + api_key
 
 # Make a GET request to the News API
@@ -23,11 +23,12 @@ message = ""
 # Iterate through each article received from the API
 for article in content["articles"]:
     if article["title"] is not None:
-        # Extract the title and description of the article and append to the
-        # message
+        # Extract the title, description and url of the article and append
+        # to the message
         title = article["title"]
         description = article["description"]
-        message += f"{title}\nDescription: {description}\n\n"
+        article_url = article["url"]
+        message += f"{title}\nDescription: {description}\n{article_url}\n\n"
 
 # Send the email with all titles and descriptions as the message
 send_email(message)
